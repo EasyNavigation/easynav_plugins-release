fa21ca193d357910c1fe6ccd6824a2d2bd198d9c
Francisco Martín Rico <fmrico@gmail.com>
2026-07-26 10:59:19 +0200

    Rebase from 'upstream'

--- maps_managers/easynav_navmap_maps_manager/src/easynav_navmap_maps_manager/filters/ObstacleFilter.cpp
+++ maps_managers/easynav_navmap_maps_manager/src/easynav_navmap_maps_manager/filters/ObstacleFilter.cpp
@@ -18,7 +18,7 @@
 #include <cstdint>
 
 #include "easynav_common/types/NavState.hpp"
-#include "easynav_common/types/PointPerception.hpp"
+#include "easynav_sensors/types/PointPerception.hpp"
 #include "easynav_common/RTTFBuffer.hpp"
 
 #include "navmap_core/NavMap.hpp"
@@ -44,9 +44,13 @@ ObstacleFilter::on_initialize()
 void ObstacleFilter::update(::easynav::NavState & nav_state)
 {
   if (!nav_state.has("map.navmap")) {return;}
-  if (!nav_state.has("points")) {return;}
 
-  const auto & perceptions = nav_state.get<PointPerceptions>("points");
+  const auto & perceptions = nav_state.get_no_group<PointPerception>();
+  if (perceptions.empty()) {
+    RCLCPP_WARN(get_node()->get_logger(), "There are no points perceptions");
+    return;
+  }
+
   navmap_ = nav_state.get<::navmap::NavMap>("map.navmap");
   const auto & tf_info = RTTFBuffer::getInstance()->get_tf_info();
 
